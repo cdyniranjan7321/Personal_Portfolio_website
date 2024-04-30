ca69bc569681f79d50451df5a6b006fe3eecc0a9
Niranjan Chaudhary <cdyniranjan7321@gmail.com>
2024-04-30 12:24:30 +0545

    second commit

--- lib/data.ts
+++ lib/data.ts
@@ -35,28 +35,28 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Graduated bachelor degree",
+    location: "Pokhara, Nepal",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "I graduated after 4 years of studying. I immediately found a internship as a full-stack developer.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2023",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Full-stack Developer",
+    location: "AakarEsolution software company, Pokhara ,Nepal",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "I worked as a full-stack developer.My stack includes React, Next.js, TypeScript, Tailwind, Prisma and PostgreSQL. My experience includes developing a quiz app, where I seamlessly integrated front-end and back-end technologies to deliver a smooth user experience.   ",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2023 - 2024",
   },
   {
     title: "Full-Stack Developer",
-    location: "Houston, TX",
+    location: "Pokhara, Kaski, Nepal",
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2024 - present",
   },
 ] as const;
 
